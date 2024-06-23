b6d95da84cc08cc38a4b8672cca4eb376315a6e6
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-23 13:52:23 +0700

    js lab 134

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-// write code here
+// งหาผลคูณของ element ใน nums
+
+const nums = [-3, 2, 11, -7, 4, 6];
+const multiNums = nums.reduce((muultiple, num) => muultiple * num);
+console.log(multiNums)
